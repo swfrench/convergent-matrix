846c141eea4bdc1ccf303c0783efe87730d09672
sfrench <sfrench@seismo.berkeley.edu>
2013-12-11 19:38:37 -0800

    Wrapper for wtime (MPI vs. OMP); Smaller default test
    Can skip OMP and use MPI_Wtime (yes, for clang ...).
    Smaller default test appropriate for TEST_CONSISTENCY.

--- example/run_ext_test.cpp
+++ example/run_ext_test.cpp
@@ -2,7 +2,13 @@
 #include <cstdlib>
 #include <ctime>
 #include <cassert>
+
+#ifdef USE_MPI_WTIME
+#include <mpi.h>
+#else
 #include <omp.h>
+#endif
+
 #include <upcxx.h>
 
 #include "convergent_matrix.hpp"
@@ -15,11 +21,11 @@
 #define NITER_MAX 10
 
 // 100000 for 8 x 8
-#define MSUB_REPS 10
-#define MSUB_SIZE 10000
-#define M ( MSUB_REPS * MSUB_SIZE )
-#define MSUB_MIN 1000
-#define MSUB_MAX 1200
+//#define MSUB_REPS 10
+//#define MSUB_SIZE 10000
+//#define M ( MSUB_REPS * MSUB_SIZE )
+//#define MSUB_MIN 1000
+//#define MSUB_MAX 1200
 
 // 25000 for 2 x 2
 //#define MSUB_REPS 10
@@ -28,6 +34,13 @@
 //#define MSUB_MIN 250
 //#define MSUB_MAX 300
 
+// 10000 for 2 x 2
+#define MSUB_REPS 10
+#define MSUB_SIZE 1000
+#define M ( MSUB_REPS * MSUB_SIZE )
+#define MSUB_MIN 100
+#define MSUB_MAX 120
+
 #include "tests/test01.hpp"
 
 /**
@@ -35,14 +48,24 @@
  */
 
 // block-cyclic distribution
-#define NPCOL 8
-#define NPROW 8
+#define NPCOL 2
+#define NPROW 2
 #define MB 64
 #define NB 64
 #define LLD 12544
 typedef cm::ConvergentMatrix<double,NPROW,NPCOL,MB,NB,LLD> cmat_t;
 
 using namespace std;
+
+double
+get_wtime()
+{
+#ifdef USE_MPI_WTIME
+  return MPI_Wtime();
+#else
+  return omp_get_wtime();
+#endif
+}
 
 int
 main( int argc, char **argv )
@@ -57,6 +80,12 @@ main( int argc, char **argv )
   upcxx::init( &argc, &argv );
 
   assert( THREADS == NPROW * NPCOL );
+
+  // init MPI for tests
+  int mpi_init;
+  MPI_Initialized( &mpi_init );
+  if ( ! mpi_init )
+    MPI_Init( &argc, &argv );
 
   printf( "%4i : up - using test \"%s\"\n", MYTHREAD, TESTNAME ); fflush( stdout );
 
@@ -82,18 +111,18 @@ main( int argc, char **argv )
       cm::LocalMatrix<double> *GtG;
       GtG = new cm::LocalMatrix<double>( nxs[r], nxs[r], data );
       // track update time
-      wt_tot -= omp_get_wtime();
+      wt_tot -= get_wtime();
       dist_mat->update( GtG, ixs[r] );
-      wt_tot += omp_get_wtime();
+      wt_tot += get_wtime();
       delete GtG;
     }
 
   // freeze the ConvergentMatrix abstraction
   printf( "%4i : freezing ...\n", MYTHREAD ); fflush( stdout );
   // track freeze time
-  wt_tot -= omp_get_wtime();
+  wt_tot -= get_wtime();
   dist_mat->freeze();
-  wt_tot += omp_get_wtime();
+  wt_tot += get_wtime();
   printf( "%4i : total time spent in update / freeze %fs\n", MYTHREAD, wt_tot ); fflush( stdout );
 
   // fetch the local PBLAS-compatible block-cyclic storage array
