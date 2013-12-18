29089a9c80c1b787e0dec9a32251d2ef5e596d83
sfrench <sfrench@seismo.berkeley.edu>
2013-12-17 22:14:03 -0800

    LocalMatrix and ConvergentMatrix to separate header files

--- include/convergent_matrix.hpp
+++ include/convergent_matrix.hpp
@@ -3,11 +3,6 @@
  *
  * Key components:
  *
- *  - The LocalMatrix<T> object, which simply represents a local matrix and
- *    supports a few common BLAS operations; these are how the updates are
- *    represented locally, along with index arrays which map into the global
- *    index space.
- *
  *  - The ConvergentMatrix<T,NPROW,NPCOL,MB,NB,LLD> abstraction accumulates
  *    updates to the global distributed matrix in bins (Bin<T>) for later
  *    asynchronous application.
@@ -15,6 +10,10 @@
  *  - The Bin<T> object implements the binning concept in ConvergentMatrix,
  *    and handles "flushing" its contents by triggering remote asynchronous
  *    updates (update_task<T>).
+ *
+ * Updates are represented as LocalMatrix<T> objects (see local_matrix.hpp),
+ * along with indexing arrays which map into the global distributed index
+ * space.
  *
  * Once all updates have been applied, the matrix can be "frozen" and all bins
  * are flushed. Thereafter, each thread has its own PBLAS-compatible portion of
@@ -25,9 +24,14 @@
 
 #pragma once
 
+#if ( defined(DEBUG_MSGS)       || \
+      defined(ASYNC_DEBUG_MSGS) || \
+      defined(TEST_CONSISTENCY) )
 #include <iostream>
+#endif
 #include <vector>
 #include <cassert>
+
 #include <upcxx.h>
 
 #ifdef TEST_CONSISTENCY
@@ -35,243 +39,24 @@
 #include <mpi.h>
 #endif
 
-// overloaded definitions of gemm() and gemv() for double and float
-#include "blas.hpp"
+// LocalMatrix<T>
+#include "local_matrix.hpp"
 
 // default bin-size threshold (number of elems) before it is flushed
+#ifndef DEFAULT_BIN_FLUSH_THRESHOLD
 #define DEFAULT_BIN_FLUSH_THRESHOLD 10000
+#endif
 
 // default number of update() to trigger progress() and drain (local) task queue
-#define DEFAULT_PROGRESS_INTERVAL 10
+#ifndef DEFAULT_PROGRESS_INTERVAL
+#define DEFAULT_PROGRESS_INTERVAL 1
+#endif
 
 /**
  * Contains classes associated with the convergent-matrix abstraction
  */
 namespace cm
 {
-
-  /**
-   * A local dense matrix abstraction supporting a limited set of level 2 and 3
-   * BLAS operations and element-wise arithmetic
-   */
-  template <typename T>
-  class LocalMatrix
-  {
-
-   private:
-
-    long _m, _n, _ld;
-    bool _alloc;
-    bool _trans;
-    T * _data;
-
-   public:
-
-    LocalMatrix() :
-      _m(0), _n(0), _ld(0), _alloc(false), _trans(false), _data(NULL)
-    {}
-
-    LocalMatrix( long m, long n ) :
-      _m(m), _n(n), _ld(m), _alloc(true), _trans(false), _data(new T[m * n])
-    {}
-
-    LocalMatrix( long m, long n, T * data ) :
-      _m(m), _n(n), _ld(m), _alloc(false), _trans(false), _data(data)
-    {}
-
-    LocalMatrix( long m, long n, long ld ) :
-      _m(m), _n(n), _ld(ld), _alloc(true), _trans(false), _data(new T[ld * n])
-    {}
-
-    LocalMatrix( long m, long n, long ld, T * data ) :
-      _m(m), _n(n), _ld(ld), _alloc(false), _trans(false), _data(data)
-    {}
-
-    ~LocalMatrix()
-    {
-      if ( _alloc )
-        delete [] _data;
-    }
-
-    inline void
-    override_free()
-    {
-      _alloc = true;
-    }
-
-    inline long
-    m() const
-    {
-      return _trans ? _n : _m;
-    }
-
-    inline long
-    n() const
-    {
-      return _trans ? _m : _n;
-    }
-
-    inline T *
-    data() const
-    {
-      return _data;
-    }
-
-    LocalMatrix<T> *
-    trans()
-    {
-      LocalMatrix<T> * C = new LocalMatrix<T>( _m, _n, _ld, _data );
-      C->_trans = ! _trans;
-      return C;
-    }
-
-    inline T&
-    operator()( long i, long j ) const
-    {
-#ifndef NOCHECK
-      assert( i >= 0 );
-      assert( j >= 0 );
-      assert( i < m() );
-      assert( j < n() );
-#endif
-      return _trans ? _data[j + i * _ld] : _data[i + _ld * j];
-    }
-
-    inline T&
-    operator()( long i ) const
-    {
-#ifndef NOCHECK
-      assert( ( m() == 1 &&   _trans ) ||
-              ( n() == 1 && ! _trans ) );
-#endif
-      return _data[i];
-    }
-
-    LocalMatrix<T> &
-    operator=( T val )
-    {
-      for ( long j = 0; j < n(); j++ )
-        for ( long i = 0; i < m(); i++ )
-          (*this)( i, j ) = val;
-      return *this;
-    }
-
-    LocalMatrix<T> *
-    operator+( LocalMatrix<T> &B ) const
-    {
-      LocalMatrix<T> *C = new LocalMatrix<T>( _m, _n, _ld );
-      if ( _trans )
-        C->trans();
-#ifndef NOCHECK
-      assert( m() == B.m() );
-      assert( n() == B.n() );
-#endif
-      for ( long j = 0; j < n(); j++ )
-        for ( long i = 0; i < m(); i++ )
-          (*C)( i, j ) = (*this)( i, j ) + B( i, j );
-      return C;
-    }
-
-    LocalMatrix<T> *
-    operator-( LocalMatrix<T> &B ) const
-    {
-      LocalMatrix<T> *C = new LocalMatrix<T>( _m, _n, _ld );
-      if ( _trans )
-        C->trans();
-#ifndef NOCHECK
-      assert( m() == B.m() );
-      assert( n() == B.n() );
-#endif
-      for ( long j = 0; j < n(); j++ )
-        for ( long i = 0; i < m(); i++ )
-          (*C)( i, j ) = (*this)( i, j ) - B( i, j );
-      return C;
-    }
-
-    LocalMatrix<T> &
-    operator+=( LocalMatrix<T> &B )
-    {
-#ifndef NOCHECK
-      assert( m() == B.m() );
-      assert( n() == B.n() );
-#endif
-      for ( long j = 0; j < n(); j++ )
-        for ( long i = 0; i < m(); i++ )
-          (*this)( i, j ) += B( i, j );
-      return *this;
-    }
-
-    LocalMatrix<T> &
-    operator-=( LocalMatrix<T> &B )
-    {
-#ifndef NOCHECK
-      assert( m() == B.m() );
-      assert( n() == B.n() );
-#endif
-      for ( long j = 0; j < n(); j++ )
-        for ( long i = 0; i < m(); i++ )
-          (*this)( i, j ) -= B( i, j );
-      return *this;
-    }
-
-    LocalMatrix<T> *
-    operator*( LocalMatrix<T> &B ) const
-    {
-      LocalMatrix<T> *C;
-      T alpha = 1.0, beta = 0.0;
-
-      // check on _view_ dimenions
-      assert( n() == B.m() );
-
-      C = new LocalMatrix<T>( m(), B.n() );
-
-      if ( B.n() == 1 )
-        {
-#ifndef NOCHECK
-          assert( ! B._trans ||
-                  ( B._trans && B._ld == 1 ) );
-#endif
-          char transa;
-          int m, n, lda;
-          int one = 1;
-          m = _m; // true rows of A
-          n = _n; // true cols of A
-          lda = _ld;
-          transa = _trans ? 'T' : 'N';
-          gemv( &transa,
-                &m, &n,
-                &alpha,
-                _data, &lda,
-                B._data, &one,
-                &beta,
-                C->_data, &one );
-        }
-      else
-        {
-          int m, n, k, lda, ldb, ldc;
-          char transa, transb;
-          m = this->m(); // rows of op( A )
-          n = B.n();     // cols of op( B )
-          k = this->n(); // cols of op( A )
-          lda = _ld;
-          ldb = B._ld;
-          ldc = C->_ld;
-          transa = _trans ? 'T' : 'N';
-          transb = B._trans ? 'T' : 'N';
-          gemm( &transa, &transb,
-                &m, &n, &k,
-                &alpha,
-                _data, &lda,
-                B._data, &ldb,
-                &beta,
-                C->_data, &ldc );
-        }
-
-      return C;
-    }
-
-  }; // end of LocalMatrix
-
 
   /**
    * A task that performs remote updates (spawned by Bin<T>)
---
+++ include/local_matrix.hpp
@@ -0,0 +1,238 @@
+/**
+ * The LocalMatrix<T> object, which simply represents a local matrix and
+ * supports a few common BLAS operations; these are how matrix updates are
+ * represented locally, along with index arrays which map into the global
+ * index space.
+ */
+
+#pragma once
+
+// overloaded definitions of gemm() and gemv() for double and float
+#include "blas.hpp"
+
+namespace cm
+{
+
+  /**
+   * A local dense matrix abstraction supporting a limited set of level 2 and 3
+   * BLAS operations and element-wise arithmetic
+   */
+  template <typename T>
+  class LocalMatrix
+  {
+
+   private:
+
+    long _m, _n, _ld;
+    bool _alloc;
+    bool _trans;
+    T * _data;
+
+   public:
+
+    LocalMatrix() :
+      _m(0), _n(0), _ld(0), _alloc(false), _trans(false), _data(NULL)
+    {}
+
+    LocalMatrix( long m, long n ) :
+      _m(m), _n(n), _ld(m), _alloc(true), _trans(false), _data(new T[m * n])
+    {}
+
+    LocalMatrix( long m, long n, T * data ) :
+      _m(m), _n(n), _ld(m), _alloc(false), _trans(false), _data(data)
+    {}
+
+    LocalMatrix( long m, long n, long ld ) :
+      _m(m), _n(n), _ld(ld), _alloc(true), _trans(false), _data(new T[ld * n])
+    {}
+
+    LocalMatrix( long m, long n, long ld, T * data ) :
+      _m(m), _n(n), _ld(ld), _alloc(false), _trans(false), _data(data)
+    {}
+
+    ~LocalMatrix()
+    {
+      if ( _alloc )
+        delete [] _data;
+    }
+
+    inline void
+    override_free()
+    {
+      _alloc = true;
+    }
+
+    inline long
+    m() const
+    {
+      return _trans ? _n : _m;
+    }
+
+    inline long
+    n() const
+    {
+      return _trans ? _m : _n;
+    }
+
+    inline T *
+    data() const
+    {
+      return _data;
+    }
+
+    LocalMatrix<T> *
+    trans()
+    {
+      LocalMatrix<T> * C = new LocalMatrix<T>( _m, _n, _ld, _data );
+      C->_trans = ! _trans;
+      return C;
+    }
+
+    inline T&
+    operator()( long i, long j ) const
+    {
+#ifndef NOCHECK
+      assert( i >= 0 );
+      assert( j >= 0 );
+      assert( i < m() );
+      assert( j < n() );
+#endif
+      return _trans ? _data[j + i * _ld] : _data[i + _ld * j];
+    }
+
+    inline T&
+    operator()( long i ) const
+    {
+#ifndef NOCHECK
+      assert( ( m() == 1 &&   _trans ) ||
+              ( n() == 1 && ! _trans ) );
+#endif
+      return _data[i];
+    }
+
+    LocalMatrix<T> &
+    operator=( T val )
+    {
+      for ( long j = 0; j < n(); j++ )
+        for ( long i = 0; i < m(); i++ )
+          (*this)( i, j ) = val;
+      return *this;
+    }
+
+    LocalMatrix<T> *
+    operator+( LocalMatrix<T> &B ) const
+    {
+      LocalMatrix<T> *C = new LocalMatrix<T>( _m, _n, _ld );
+      if ( _trans )
+        C->trans();
+#ifndef NOCHECK
+      assert( m() == B.m() );
+      assert( n() == B.n() );
+#endif
+      for ( long j = 0; j < n(); j++ )
+        for ( long i = 0; i < m(); i++ )
+          (*C)( i, j ) = (*this)( i, j ) + B( i, j );
+      return C;
+    }
+
+    LocalMatrix<T> *
+    operator-( LocalMatrix<T> &B ) const
+    {
+      LocalMatrix<T> *C = new LocalMatrix<T>( _m, _n, _ld );
+      if ( _trans )
+        C->trans();
+#ifndef NOCHECK
+      assert( m() == B.m() );
+      assert( n() == B.n() );
+#endif
+      for ( long j = 0; j < n(); j++ )
+        for ( long i = 0; i < m(); i++ )
+          (*C)( i, j ) = (*this)( i, j ) - B( i, j );
+      return C;
+    }
+
+    LocalMatrix<T> &
+    operator+=( LocalMatrix<T> &B )
+    {
+#ifndef NOCHECK
+      assert( m() == B.m() );
+      assert( n() == B.n() );
+#endif
+      for ( long j = 0; j < n(); j++ )
+        for ( long i = 0; i < m(); i++ )
+          (*this)( i, j ) += B( i, j );
+      return *this;
+    }
+
+    LocalMatrix<T> &
+    operator-=( LocalMatrix<T> &B )
+    {
+#ifndef NOCHECK
+      assert( m() == B.m() );
+      assert( n() == B.n() );
+#endif
+      for ( long j = 0; j < n(); j++ )
+        for ( long i = 0; i < m(); i++ )
+          (*this)( i, j ) -= B( i, j );
+      return *this;
+    }
+
+    LocalMatrix<T> *
+    operator*( LocalMatrix<T> &B ) const
+    {
+      LocalMatrix<T> *C;
+      T alpha = 1.0, beta = 0.0;
+
+      // check on _view_ dimenions
+      assert( n() == B.m() );
+
+      C = new LocalMatrix<T>( m(), B.n() );
+
+      if ( B.n() == 1 )
+        {
+#ifndef NOCHECK
+          assert( ! B._trans ||
+                  ( B._trans && B._ld == 1 ) );
+#endif
+          char transa;
+          int m, n, lda;
+          int one = 1;
+          m = _m; // true rows of A
+          n = _n; // true cols of A
+          lda = _ld;
+          transa = _trans ? 'T' : 'N';
+          gemv( &transa,
+                &m, &n,
+                &alpha,
+                _data, &lda,
+                B._data, &one,
+                &beta,
+                C->_data, &one );
+        }
+      else
+        {
+          int m, n, k, lda, ldb, ldc;
+          char transa, transb;
+          m = this->m(); // rows of op( A )
+          n = B.n();     // cols of op( B )
+          k = this->n(); // cols of op( A )
+          lda = _ld;
+          ldb = B._ld;
+          ldc = C->_ld;
+          transa = _trans ? 'T' : 'N';
+          transb = B._trans ? 'T' : 'N';
+          gemm( &transa, &transb,
+                &m, &n, &k,
+                &alpha,
+                _data, &lda,
+                B._data, &ldb,
+                &beta,
+                C->_data, &ldc );
+        }
+
+      return C;
+    }
+
+  }; // end of LocalMatrix
+
+} // end of namespace cm
