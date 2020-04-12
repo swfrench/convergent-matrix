9a190c0d24e8d7f6bc7616b79b23ac7631e6a5e0
swfrench <scott.w.french@gmail.com>
2020-04-12 10:54:29 -0700

    Bulk clang-format of header files

--- include/bin.hpp
+++ include/bin.hpp
@@ -9,186 +9,158 @@
 
 #include <upcxx/upcxx.hpp>
 
-namespace cm
-{
+namespace cm {
 
-  /// @cond INTERNAL_DOCS
+/// @cond INTERNAL_DOCS
 
-  /**
-   * A task that performs remote updates (spawned by Bin<T>)
-   * \param g_my_data Reference to local storage on the target
-   * \param ix View to update indexing (already transferred target)
-   * \param data View to update data (already transferred target)
-   */
-  template <typename T>
-  void
-  update_task( long size,
-               upcxx::global_ptr<T> g_my_data,
-               upcxx::view<long>    ix,
-               upcxx::view<T>       data )
-  {
+/**
+ * A task that performs remote updates (spawned by Bin<T>)
+ * \param g_my_data Reference to local storage on the target
+ * \param ix View to update indexing (already transferred target)
+ * \param data View to update data (already transferred target)
+ */
+template <typename T>
+void update_task(long size, upcxx::global_ptr<T> g_my_data,
+                 upcxx::view<long> ix, upcxx::view<T> data) {
 #ifndef NOCHECK
-    assert( g_my_data.is_local() );
+  assert(g_my_data.is_local());
 #endif
-    T* my_data = g_my_data.local();
-    for ( long k = 0; k < size; k++ )
-      my_data[ix[k]] += data[k];
+  T *my_data = g_my_data.local();
+  for (long k = 0; k < size; k++) my_data[ix[k]] += data[k];
+}
+
+/**
+ * Implements the binning / remote application for a single thread
+ * Not very efficient in terms of space for the moment, in exchange for
+ * simplicity.
+ * \tparam T Matrix data type (e.g. float)
+ */
+template <typename T>
+class Bin {
+ private:
+  bool _init;                           // whether fields intialized
+  int _remote_tid;                      // thread id of target
+  upcxx::global_ptr<T> _g_remote_data;  // global_ptr _local_ to target
+  std::vector<long> _ix;                // linear indexing for target
+  std::vector<T> _data;                 // update data for target
+#ifdef ENABLE_PROGRESS_THREAD
+  pthread_mutex_t *_tq_mutex;  // mutex protecting task-queue ops
+#endif
+
+  inline void clear() {
+    _ix.clear();
+    _data.clear();
   }
 
+ public:
+  /**
+   * Create an uninitialized Bin object
+   */
+  Bin() : _init(false) {}
+
+#ifdef ENABLE_PROGRESS_THREAD
+  /**
+   * Create the Bin object associated with a given target
+   * \param g_remote_data global_ptr<T> reference to the target-local storage
+   * \param tq_mutex pthread mutex protecting the task queue and associated
+   * GASNet operations
+   */
+  Bin(upcxx::global_ptr<T> g_remote_data, pthread_mutex_t *tq_mutex)
+      : _init(true),
+        _remote_tid(g_remote_data.where()),
+        _g_remote_data(g_remote_data),
+        _tq_mutex(tq_mutex) {}
 
   /**
-   * Implements the binning / remote application for a single thread
-   * Not very efficient in terms of space for the moment, in exchange for
-   * simplicity.
-   * \tparam T Matrix data type (e.g. float)
+   * Initialize the previously empty Bin object for a given target
+   * \param g_remote_data global_ptr<T> reference to the target-local storage
+   * \param tq_mutex pthread mutex protecting the task queue and associated
+   * GASNet operations
+   *
+   * \b Note: It is erroneous to call this on a previously initialized Bin.
    */
-  template<typename T>
-  class Bin
-  {
-
-   private:
-
-    bool _init;                           // whether fields intialized
-    int _remote_tid;                      // thread id of target
-    upcxx::global_ptr<T> _g_remote_data;  // global_ptr _local_ to target
-    std::vector<long> _ix;                // linear indexing for target
-    std::vector<T> _data;                 // update data for target
-#ifdef ENABLE_PROGRESS_THREAD
-    pthread_mutex_t *_tq_mutex;           // mutex protecting task-queue ops
-#endif
-
-    inline void
-    clear()
-    {
-      _ix.clear();
-      _data.clear();
-    }
-
-   public:
-
-    /**
-     * Create an uninitialized Bin object
-     */
-    Bin() :
-      _init(false)
-    {}
-
-#ifdef ENABLE_PROGRESS_THREAD
-    /**
-     * Create the Bin object associated with a given target
-     * \param g_remote_data global_ptr<T> reference to the target-local storage
-     * \param tq_mutex pthread mutex protecting the task queue and associated
-     * GASNet operations
-     */
-    Bin( upcxx::global_ptr<T> g_remote_data, pthread_mutex_t * tq_mutex ) :
-      _init(true),
-      _remote_tid(g_remote_data.where()),
-      _g_remote_data(g_remote_data),
-      _tq_mutex(tq_mutex)
-    {}
-
-    /**
-     * Initialize the previously empty Bin object for a given target
-     * \param g_remote_data global_ptr<T> reference to the target-local storage
-     * \param tq_mutex pthread mutex protecting the task queue and associated
-     * GASNet operations
-     *
-     * \b Note: It is erroneous to call this on a previously initialized Bin.
-     */
-    inline void
-    init( upcxx::global_ptr<T> g_remote_data, pthread_mutex_t * tq_mutex )
-    {
-      assert( ! _init );
-      _remote_tid = g_remote_data.where();
-      _g_remote_data = g_remote_data;
-      _tq_mutex = tq_mutex;
-      _init = true;
-    }
+  inline void init(upcxx::global_ptr<T> g_remote_data,
+                   pthread_mutex_t *tq_mutex) {
+    assert(!_init);
+    _remote_tid = g_remote_data.where();
+    _g_remote_data = g_remote_data;
+    _tq_mutex = tq_mutex;
+    _init = true;
+  }
 #else
-    /**
-     * Create the Bin object associated with a given target
-     * \param g_remote_data global_ptr<T> reference to the target-local storage
-     */
-    Bin( upcxx::global_ptr<T> g_remote_data ) :
-      _init(true),
-      _remote_tid(g_remote_data.where()),
-      _g_remote_data(g_remote_data)
-    {}
+  /**
+   * Create the Bin object associated with a given target
+   * \param g_remote_data global_ptr<T> reference to the target-local storage
+   */
+  Bin(upcxx::global_ptr<T> g_remote_data)
+      : _init(true),
+        _remote_tid(g_remote_data.where()),
+        _g_remote_data(g_remote_data) {}
 
-    /**
-     * Initialize the previously empty Bin object for a given target
-     * \param g_remote_data global_ptr<T> reference to the target-local storage
-     *
-     * \b Note: It is erroneous to call this on a previously initialized Bin.
-     */
-    inline void
-    init( upcxx::global_ptr<T> g_remote_data )
-    {
-      assert( ! _init );
-      _remote_tid = g_remote_data.where();
-      _g_remote_data = g_remote_data;
-      _init = true;
-    }
+  /**
+   * Initialize the previously empty Bin object for a given target
+   * \param g_remote_data global_ptr<T> reference to the target-local storage
+   *
+   * \b Note: It is erroneous to call this on a previously initialized Bin.
+   */
+  inline void init(upcxx::global_ptr<T> g_remote_data) {
+    assert(!_init);
+    _remote_tid = g_remote_data.where();
+    _g_remote_data = g_remote_data;
+    _init = true;
+  }
 #endif
 
-    /**
-     * Current size of the bin (number of update elems not yet applied)
-     */
-    inline long
-    size() const
-    {
-      return _ix.size();
-    }
+  /**
+   * Current size of the bin (number of update elems not yet applied)
+   */
+  inline long size() const { return _ix.size(); }
 
-    /**
-     * Add an elemental update to this bin
-     * \param data Elemental update (r.h.s. of +=)
-     * \param ij Linear index (on the target) of element to be updated
-     */
-    inline void
-    append( T data, long ij )
-    {
-      _ix.push_back( ij );
-      _data.push_back( data );
-    }
+  /**
+   * Add an elemental update to this bin
+   * \param data Elemental update (r.h.s. of +=)
+   * \param ij Linear index (on the target) of element to be updated
+   */
+  inline void append(T data, long ij) {
+    _ix.push_back(ij);
+    _data.push_back(data);
+  }
 
-    /**
-     * "Flush" this bin by initiate remote async update using the current bin
-     * contents.
-     * \param p_op upcxx::promise<> pointer to which RPC remote operation
-     * completion will be registered
-     */
-    void
-    flush( upcxx::promise<> *p_op )
-    {
+  /**
+   * "Flush" this bin by initiate remote async update using the current bin
+   * contents.
+   * \param p_op upcxx::promise<> pointer to which RPC remote operation
+   * completion will be registered
+   */
+  void flush(upcxx::promise<> *p_op) {
 #ifndef NOCHECK
-      assert( _init );
+    assert(_init);
 #endif
 
 #ifdef ENABLE_PROGRESS_THREAD
-      pthread_mutex_lock( _tq_mutex );
+    pthread_mutex_lock(_tq_mutex);
 #endif
 
-      upcxx::promise<> p_src;  // Track source completion to render clear() safe
+    upcxx::promise<> p_src;  // Track source completion to render clear() safe
 
-      upcxx::rpc( _remote_tid,
-                  upcxx::source_cx::as_promise( p_src ) |
-                  upcxx::operation_cx::as_promise( *p_op ),
-                  update_task<T>, _data.size(), _g_remote_data,
-                  upcxx::make_view( _ix ), upcxx::make_view( _data ));
+    upcxx::rpc(_remote_tid,
+               upcxx::source_cx::as_promise(p_src) |
+                   upcxx::operation_cx::as_promise(*p_op),
+               update_task<T>, _data.size(), _g_remote_data,
+               upcxx::make_view(_ix), upcxx::make_view(_data));
 
-      p_src.finalize().wait();
+    p_src.finalize().wait();
 
 #ifdef ENABLE_PROGRESS_THREAD
-      pthread_mutex_unlock( _tq_mutex );
+    pthread_mutex_unlock(_tq_mutex);
 #endif
 
-      // clear internal (vector) storage
-      clear();
-    }
+    // clear internal (vector) storage
+    clear();
+  }
 
-  }; // end of Bin
+};  // end of Bin
 
-  /// @endcond
+/// @endcond
 
-}
+}  // namespace cm
--- include/local_matrix.hpp
+++ include/local_matrix.hpp
@@ -3,229 +3,171 @@
 // overloaded definitions of gemm() and gemv() for double and float
 #include "blas.hpp"
 
-namespace cm
-{
+namespace cm {
 
-  /**
-   * A local dense matrix abstraction supporting a limited set of level 2 and 3
-   * BLAS operations and element-wise arithmetic
-   */
-  template <typename T>
-  class LocalMatrix
-  {
+/**
+ * A local dense matrix abstraction supporting a limited set of level 2 and 3
+ * BLAS operations and element-wise arithmetic
+ */
+template <typename T>
+class LocalMatrix {
+ private:
+  long _m, _n, _ld;
+  bool _alloc;
+  bool _trans;
+  T *_data;
 
-   private:
+ public:
+  LocalMatrix()
+      : _m(0), _n(0), _ld(0), _alloc(false), _trans(false), _data(NULL) {}
 
-    long _m, _n, _ld;
-    bool _alloc;
-    bool _trans;
-    T * _data;
+  LocalMatrix(long m, long n)
+      : _m(m),
+        _n(n),
+        _ld(m),
+        _alloc(true),
+        _trans(false),
+        _data(new T[m * n]) {}
 
-   public:
+  LocalMatrix(long m, long n, T *data)
+      : _m(m), _n(n), _ld(m), _alloc(false), _trans(false), _data(data) {}
 
-    LocalMatrix() :
-      _m(0), _n(0), _ld(0), _alloc(false), _trans(false), _data(NULL)
-    {}
+  LocalMatrix(long m, long n, long ld)
+      : _m(m),
+        _n(n),
+        _ld(ld),
+        _alloc(true),
+        _trans(false),
+        _data(new T[ld * n]) {}
 
-    LocalMatrix( long m, long n ) :
-      _m(m), _n(n), _ld(m), _alloc(true), _trans(false), _data(new T[m * n])
-    {}
+  LocalMatrix(long m, long n, long ld, T *data)
+      : _m(m), _n(n), _ld(ld), _alloc(false), _trans(false), _data(data) {}
 
-    LocalMatrix( long m, long n, T * data ) :
-      _m(m), _n(n), _ld(m), _alloc(false), _trans(false), _data(data)
-    {}
+  ~LocalMatrix() {
+    if (_alloc) delete[] _data;
+  }
 
-    LocalMatrix( long m, long n, long ld ) :
-      _m(m), _n(n), _ld(ld), _alloc(true), _trans(false), _data(new T[ld * n])
-    {}
+  inline void override_free() { _alloc = true; }
 
-    LocalMatrix( long m, long n, long ld, T * data ) :
-      _m(m), _n(n), _ld(ld), _alloc(false), _trans(false), _data(data)
-    {}
+  inline long m() const { return _trans ? _n : _m; }
 
-    ~LocalMatrix()
-    {
-      if ( _alloc )
-        delete [] _data;
-    }
+  inline long n() const { return _trans ? _m : _n; }
 
-    inline void
-    override_free()
-    {
-      _alloc = true;
-    }
+  inline T *data() const { return _data; }
 
-    inline long
-    m() const
-    {
-      return _trans ? _n : _m;
-    }
+  LocalMatrix<T> *trans() {
+    LocalMatrix<T> *C = new LocalMatrix<T>(_m, _n, _ld, _data);
+    C->_trans = !_trans;
+    return C;
+  }
 
-    inline long
-    n() const
-    {
-      return _trans ? _m : _n;
-    }
-
-    inline T *
-    data() const
-    {
-      return _data;
-    }
-
-    LocalMatrix<T> *
-    trans()
-    {
-      LocalMatrix<T> * C = new LocalMatrix<T>( _m, _n, _ld, _data );
-      C->_trans = ! _trans;
-      return C;
-    }
-
-    inline T&
-    operator()( long i, long j ) const
-    {
+  inline T &operator()(long i, long j) const {
 #ifndef NOCHECK
-      assert( i >= 0 );
-      assert( j >= 0 );
-      assert( i < m() );
-      assert( j < n() );
+    assert(i >= 0);
+    assert(j >= 0);
+    assert(i < m());
+    assert(j < n());
 #endif
-      return _trans ? _data[j + i * _ld] : _data[i + _ld * j];
-    }
+    return _trans ? _data[j + i * _ld] : _data[i + _ld * j];
+  }
 
-    inline T&
-    operator()( long i ) const
-    {
+  inline T &operator()(long i) const {
 #ifndef NOCHECK
-      assert( ( m() == 1 &&   _trans ) ||
-              ( n() == 1 && ! _trans ) );
+    assert((m() == 1 && _trans) || (n() == 1 && !_trans));
 #endif
-      return _data[i];
-    }
+    return _data[i];
+  }
 
-    LocalMatrix<T> &
-    operator=( T val )
-    {
-      for ( long j = 0; j < n(); j++ )
-        for ( long i = 0; i < m(); i++ )
-          (*this)( i, j ) = val;
-      return *this;
-    }
+  LocalMatrix<T> &operator=(T val) {
+    for (long j = 0; j < n(); j++)
+      for (long i = 0; i < m(); i++) (*this)(i, j) = val;
+    return *this;
+  }
 
-    LocalMatrix<T> *
-    operator+( LocalMatrix<T> &B ) const
-    {
-      LocalMatrix<T> *C = new LocalMatrix<T>( _m, _n, _ld );
-      if ( _trans )
-        C->trans();
+  LocalMatrix<T> *operator+(LocalMatrix<T> &B) const {
+    LocalMatrix<T> *C = new LocalMatrix<T>(_m, _n, _ld);
+    if (_trans) C->trans();
 #ifndef NOCHECK
-      assert( m() == B.m() );
-      assert( n() == B.n() );
+    assert(m() == B.m());
+    assert(n() == B.n());
 #endif
-      for ( long j = 0; j < n(); j++ )
-        for ( long i = 0; i < m(); i++ )
-          (*C)( i, j ) = (*this)( i, j ) + B( i, j );
-      return C;
-    }
+    for (long j = 0; j < n(); j++)
+      for (long i = 0; i < m(); i++) (*C)(i, j) = (*this)(i, j) + B(i, j);
+    return C;
+  }
 
-    LocalMatrix<T> *
-    operator-( LocalMatrix<T> &B ) const
-    {
-      LocalMatrix<T> *C = new LocalMatrix<T>( _m, _n, _ld );
-      if ( _trans )
-        C->trans();
+  LocalMatrix<T> *operator-(LocalMatrix<T> &B) const {
+    LocalMatrix<T> *C = new LocalMatrix<T>(_m, _n, _ld);
+    if (_trans) C->trans();
 #ifndef NOCHECK
-      assert( m() == B.m() );
-      assert( n() == B.n() );
+    assert(m() == B.m());
+    assert(n() == B.n());
 #endif
-      for ( long j = 0; j < n(); j++ )
-        for ( long i = 0; i < m(); i++ )
-          (*C)( i, j ) = (*this)( i, j ) - B( i, j );
-      return C;
-    }
+    for (long j = 0; j < n(); j++)
+      for (long i = 0; i < m(); i++) (*C)(i, j) = (*this)(i, j) - B(i, j);
+    return C;
+  }
 
-    LocalMatrix<T> &
-    operator+=( LocalMatrix<T> &B )
-    {
+  LocalMatrix<T> &operator+=(LocalMatrix<T> &B) {
 #ifndef NOCHECK
-      assert( m() == B.m() );
-      assert( n() == B.n() );
+    assert(m() == B.m());
+    assert(n() == B.n());
 #endif
-      for ( long j = 0; j < n(); j++ )
-        for ( long i = 0; i < m(); i++ )
-          (*this)( i, j ) += B( i, j );
-      return *this;
-    }
+    for (long j = 0; j < n(); j++)
+      for (long i = 0; i < m(); i++) (*this)(i, j) += B(i, j);
+    return *this;
+  }
 
-    LocalMatrix<T> &
-    operator-=( LocalMatrix<T> &B )
-    {
+  LocalMatrix<T> &operator-=(LocalMatrix<T> &B) {
 #ifndef NOCHECK
-      assert( m() == B.m() );
-      assert( n() == B.n() );
+    assert(m() == B.m());
+    assert(n() == B.n());
 #endif
-      for ( long j = 0; j < n(); j++ )
-        for ( long i = 0; i < m(); i++ )
-          (*this)( i, j ) -= B( i, j );
-      return *this;
-    }
+    for (long j = 0; j < n(); j++)
+      for (long i = 0; i < m(); i++) (*this)(i, j) -= B(i, j);
+    return *this;
+  }
 
-    LocalMatrix<T> *
-    operator*( LocalMatrix<T> &B ) const
-    {
-      LocalMatrix<T> *C;
-      T alpha = 1.0, beta = 0.0;
+  LocalMatrix<T> *operator*(LocalMatrix<T> &B) const {
+    LocalMatrix<T> *C;
+    T alpha = 1.0, beta = 0.0;
 
-      // check on _view_ dimenions
-      assert( n() == B.m() );
+    // check on _view_ dimenions
+    assert(n() == B.m());
 
-      C = new LocalMatrix<T>( m(), B.n() );
+    C = new LocalMatrix<T>(m(), B.n());
 
-      if ( B.n() == 1 )
-        {
+    if (B.n() == 1) {
 #ifndef NOCHECK
-          assert( ! B._trans ||
-                  ( B._trans && B._ld == 1 ) );
+      assert(!B._trans || (B._trans && B._ld == 1));
 #endif
-          char transa;
-          int m, n, lda;
-          int one = 1;
-          m = _m; // true rows of A
-          n = _n; // true cols of A
-          lda = _ld;
-          transa = _trans ? 'T' : 'N';
-          gemv( &transa,
-                &m, &n,
-                &alpha,
-                _data, &lda,
-                B._data, &one,
-                &beta,
-                C->_data, &one );
-        }
-      else
-        {
-          int m, n, k, lda, ldb, ldc;
-          char transa, transb;
-          m = this->m(); // rows of op( A )
-          n = B.n();     // cols of op( B )
-          k = this->n(); // cols of op( A )
-          lda = _ld;
-          ldb = B._ld;
-          ldc = C->_ld;
-          transa = _trans ? 'T' : 'N';
-          transb = B._trans ? 'T' : 'N';
-          gemm( &transa, &transb,
-                &m, &n, &k,
-                &alpha,
-                _data, &lda,
-                B._data, &ldb,
-                &beta,
-                C->_data, &ldc );
-        }
-
-      return C;
+      char transa;
+      int m, n, lda;
+      int one = 1;
+      m = _m;  // true rows of A
+      n = _n;  // true cols of A
+      lda = _ld;
+      transa = _trans ? 'T' : 'N';
+      gemv(&transa, &m, &n, &alpha, _data, &lda, B._data, &one, &beta, C->_data,
+           &one);
+    } else {
+      int m, n, k, lda, ldb, ldc;
+      char transa, transb;
+      m = this->m();  // rows of op( A )
+      n = B.n();      // cols of op( B )
+      k = this->n();  // cols of op( A )
+      lda = _ld;
+      ldb = B._ld;
+      ldc = C->_ld;
+      transa = _trans ? 'T' : 'N';
+      transb = B._trans ? 'T' : 'N';
+      gemm(&transa, &transb, &m, &n, &k, &alpha, _data, &lda, B._data, &ldb,
+           &beta, C->_data, &ldc);
     }
 
-  }; // end of LocalMatrix
+    return C;
+  }
 
-} // end of namespace cm
+};  // end of LocalMatrix
+
+}  // end of namespace cm
